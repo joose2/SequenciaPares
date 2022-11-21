135c55d3e3253896147d82ee71f8189be687f49d
jose <jose.silva2000@etec.sp.gov.br>
2022-11-21 20:19:35 -0300

    atividade de numeros pares

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Números pares entre 0 e ? ");
+
+int numeroInformado = Convert.ToInt32(Console.ReadLine());
+
+
+if (numeroInformado >= 1)
+
+
+{int numeroPar = 0;
+
+
+while (numeroPar <= numeroInformado)
+
+{ Console.WriteLine($"{numeroPar} ");
+
+numeroPar += 2;}}
+
+
+else
+
+
+{Console.Write($"O número {numeroInformado} não é inteiro positivo.");}
